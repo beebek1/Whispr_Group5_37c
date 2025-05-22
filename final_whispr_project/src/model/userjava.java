8a3e5c4350da16bd4c5ff9e3f0f3c6f45cc8352e
beebek1 <beebek990@gmail.com>
2025-05-22 16:48:15 +0545

    added new file

---
+++ final_whispr_project/src/model/userjava.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author bibek
+ */
+public class userjava {
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+    
+    public userjava(String username, String email, String password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+    
+}
